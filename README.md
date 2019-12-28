245341cbd1339c3b4642f4c3cec42cd32403e39b
Tomáš Klíma <tomas.klim@gmail.com>
2019-12-28 22:00:24 +0800

    Rename the game

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Phaser.js Zoo Game
-Game based e-learning game. Find the animals in the Zoo.
+# Animalearnia
+RPG level based game where the objective of each level is to learn different types of animals. The game uses game based learning principles.
+
+##### Technologies used:
+- PhaserJS 
+- Webpack
+- TypeScript
+
+##### Images used:
+- https://www.freepik.com/free-photos-vectors/design
+- https://www.kenney.nl/assets/medieval-rts
+- Pokémon GameBoy game characters
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.ts",
-  mode: "development",
-  devtool: "source-map",
+  entry: './src/index.ts',
+  mode: 'development',
+  devtool: 'source-map',
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' }
       //{ enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Phaser Zoo Game",
-      template: "./src/index.html"
+      title: 'Animalearnia',
+      template: './src/index.html'
     }),
-    new CopyWebpackPlugin([{ from: "assets", to: "assets" }])
+    new CopyWebpackPlugin([{ from: 'assets', to: 'assets' }])
   ],
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   resolve: {
-    extensions: [".ts", ".js", ".json"]
+    extensions: ['.ts', '.js', '.json']
   }
-};
+}
